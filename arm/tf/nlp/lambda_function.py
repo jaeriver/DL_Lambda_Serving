e3cddd2f70ad2b4236af9435db9fa4f57a743573
root <root@ip-172-31-55-12.us-west-2.compute.internal>
2022-01-26 05:06:19 +0000

    init arm

---
+++ arm/tf/nlp/lambda_function.py
@@ -0,0 +1,46 @@
+import time
+import boto3
+from transformers import BertTokenizer, TFBertModel
+
+def get_model(model_name, bucket_name):
+    s3_resource = boto3.resource('s3')
+    s3_bucket = s3_resource.Bucket(bucket_name)
+    
+    prefix = 'tf/' + model_name
+    for object in bucket.objects.filter(Prefix = 'dirname'):
+        if object.key == prefix:
+            os.makedirs(os.path.dirname('/tmp/' + object.key), exist_ok=True)
+            continue;
+        bucket.download_file(object.key, '/tmp/' + object.key)
+    
+    return '/tmp/' + model_name
+
+def lambda_handler(event, context):
+    bucket_name = event['bucket_name']
+    batch_size = event['batch_size']
+    model_name = event['model_name']
+    count = event['count']
+    arch_type = 'intel'
+    
+    model_path = get_model(model_name, bucket_name)
+    
+    tokenizer = BertTokenizer.from_pretrained(model_path)
+    model = TFBertModel.from_pretrained(model_path)
+    sentence = "This is Fake Dataset for testing NLP Tokenizing"
+    test_batch = [sentence for i in range(batch_size)]
+    encoded_input = tokenizer(test_batch, 
+                          return_tensors='tf',
+                          padding=True,
+                          truncation=True,
+                          max_length=512)
+    
+    time_list = []
+    for i in range(count):
+        start_time = time.time()
+        output = model(encoded_input)
+        running_time = time.time() - start_time
+        print(f"TF NLP {model_name}-{batch_size} inference latency : ",(running_time)*1000,"ms")
+        print(output)
+        time_list.append(running_time)
+    time_medium = np.median(np.array(time_list))
+    return time_medium
