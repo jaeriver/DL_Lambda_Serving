fa751ca012b96568159d2a82caf8a6fc83d196a9
최재강 <chl8273@naver.com>
2022-02-04 16:08:12 +0900

    inference work bert & image classification
    use parameters 'workload')

--- intel/tvm/lambda_function.py
+++ intel/tvm/lambda_function.py
@@ -9,24 +9,36 @@ import onnx
 import boto3
 print('import time: ', time.time() - import_start_time)
 
-def get_model(model_name, bucket_name, get_path):
+image_size = 224
+
+def get_model(model_name, bucket_name):
     s3_client = boto3.client('s3')
-    if get_path == 'tvm/':
-        print(get_path + model_name +'/model.tar')
-        s3_client.download_file(bucket_name, get_path + model_name +'/model.tar', '/tmp/model.tar')
-        s3_client.download_file(bucket_name, get_path + model_name +'/model.params', '/tmp/model.params')
-        s3_client.download_file(bucket_name, get_path + model_name +'/model.json', '/tmp/model.json')
-        return '/tmp/model.json', '/tmp/model.tar', '/tmp/model.params'
+    s3_client.download_file(bucket_name, model_name, '/tmp/'+ model_name)
+    return '/tmp/' + model_name
+
+
+def make_dataset(batch_size, workload):
+    if workload == 'image_classification":
+        image_shape = (image_size, image_size, 3)
+        data_shape = (batch_size,) + image_shape
+
+        data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+
+        return data, image_shape
+    # case bert
     else:
-        s3_client.download_file(bucket_name, get_path + model_name, '/tmp/'+ model_name)
-        return '/tmp/' + model_name
-def make_dataset(batch_size,size):
-    image_shape = (size, size, 3)
-    data_shape = (batch_size,) + image_shape
-
-    data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
-
-    return data,image_shape
+        seq_length = 128
+        shape_dict = {
+            "data0": (batch_size, seq_length),
+            "data1": (batch_size, seq_length),
+            "data2": (batch_size,),
+        }
+        input_shape = (shape_dict["data0"], shape_dict["data1"], shape_dict["data2"])
+        data = np.random.uniform(size=input_shape[0])
+        token_types = np.random.uniform(size=input_shape[1])
+        valid_length = np.array([seq_length] * batch_size)
+        
+        return data, token_types, valid_length
 
 load_model = time.time()
 
@@ -34,12 +46,13 @@ load_model = time.time()
 def lambda_handler(event, context):
     bucket_name = event['bucket_name']
     batch_size = event['batch_size']
-    onnx_name = event['model_name'] + '.onnx'
     arch_type = event['arch_type']
-    model_name = 'intel/' + event['model_name'] +'_'+str(batch_size)+'_' + arch_type
+    frame_work = event['frame_work']
+    model_name = event['model_name']
+    model_path = frame_work + arch_type + event['model_name']
     is_build = event['is_build']
     count = event['count']
-    size = 224
+    workload = event['workload']
     
     if arch_type == 'arm':
         target = tvm.target.arm_cpu()
@@ -47,31 +60,19 @@ def lambda_handler(event, context):
         target = arch_type
     ctx = tvm.cpu()
     
-    data, image_shape = make_dataset(batch_size,size)
+    loaded_lib = tvm.runtime.load_module(get_model(model_path))
+    module = runtime.GraphModule(loaded_lib["default"](ctx))
     
-    if is_build == 'true':
-        print("ONNX model imported to relay frontend.")
-        
-        onnx_model = onnx.load(get_model(onnx_name, bucket_name, 'onnx/'))
-        shape_dict = {"input_1": data.shape}
-        mod, params = relay.frontend.from_onnx(onnx_model, shape=shape_dict)
-        build_time = time.time()
-        with tvm.transform.PassContext(opt_level=3):
-            mod = relay.transform.InferType()(mod)
-            graph, lib, params = relay.build_module.build(mod, target=target, params=params)
-        print('build time:', time.time() - build_time)
-        module = graph_runtime.create(graph, lib, ctx)
-        module.set_input("input_1", data)
-        module.set_input(**params)
+    if workload == "image_classification":
+        data, image_shape = make_dataset(batch_size, workload)
+        input_name = "data"
+        module.set_input(input_name, data)
+    #case bert
     else:
-        graph_fn, mod_fn, params_fn = get_model(model_name, bucket_name, 'tvm/')
-        graph = open(graph_fn).read()
-        lib = tvm.runtime.load_module(mod_fn)
-        params = open(params_fn, "rb").read()
-        module = graph_runtime.create(graph, lib, ctx)
-        module.set_input("input_1", data)
-        module.load_params(params)
-
+        data, token_types, valid_length = make_dataset(batch_size, workload)
+        module.set_input(data0=data, data1=token_types, data2=valid_length)
+    
+    
     time_list = []
     for i in range(count):
         start_time = time.time()
