44a5a3257ec5c57561314ce4398097d316f81768
최재강 <chl8273@naver.com>
2022-02-09 22:57:32 +0900

    Update request.sh

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -1,5 +1,4 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/mxnet"
-Body='{"bucket_name": "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2", "workload": "image_classification"}'
 
 curl -X POST -H 'Content-Type: application/json' \
     -d '{ "bucket_name" : "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2", "workload": "image_classification" }' \
