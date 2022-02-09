9f8c17654ccc6f51f7e8eb759629a6c5c56862e4
최재강 <chl8273@naver.com>
2022-02-09 21:33:59 +0900

    Update request.sh

--- intel/onnx/request.sh
+++ intel/onnx/request.sh
@@ -1,4 +1,4 @@
 API_URL=https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/onnx
 Body='{"bucket_name": "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2", "workload": "image_classification"}'
 
-curl -X POST $API_URL -H 'Content-Type: application/json' -d $Body
+curl --request POST --url $API_URL -H 'Content-Type: application/json' -d $Body
