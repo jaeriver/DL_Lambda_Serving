2e6e6da317250c8624281bfa3a4dfaece9202bef
최재강 <chl8273@naver.com>
2022-02-10 07:21:12 +0900

    Update request.sh

--- intel/tvm/request.sh
+++ intel/tvm/request.sh
@@ -1,5 +1,9 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/tvm"
 
+SET=$(seq 0 4)
+for i in $SET
+do
 curl -X POST -H 'Content-Type: application/json' \
     -d '{ "bucket_name" : "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2_1.tar", "workload": "image_classification" }' \
     $API_URL
+done
