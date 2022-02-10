5ec5dce5ab0bcfca70d6b31d0886b5f372f71b2e
최재강 <chl8273@naver.com>
2022-02-10 17:01:36 +0900

    check sheel time

--- intel/onnx/request.sh
+++ intel/onnx/request.sh
@@ -1,11 +1,17 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/onnx"
-Body='{"bucket_name": "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2", "workload": "image_classification"}'
+function_name='jg-onnx-serving'
 
+aws lambda update-function-configuration \
+    --function-name $function_name \
+    --environment Variables="{model_name=$1}"
 SET=$(seq 0 4)
 for i in $SET
 do
-    curl -X POST -H 'Content-Type: application/json' \
-        -d '{ "bucket_name" : "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2.onnx", "workload": "image_classification" }' \
-        $API_URL
+start=`date +%s`
+curl -X POST -H 'Content-Type: application/json' \
+    -d '{"batch_size": 1, "workload": "image_classification" }' \
+    $API_URL
     echo ""
+end=`date +%s`
+runtime=$((end-start))
 done
