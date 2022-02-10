b2957fe85f00c62e65ab5ac4374b425bc2121cce
최재강 <chl8273@naver.com>
2022-02-10 16:28:38 +0900

    use efs

--- intel/mxnet/lambda_function.py
+++ intel/mxnet/lambda_function.py
@@ -5,17 +5,13 @@ import mxnet as mx
 import mxnet.ndarray as nd
 from mxnet import nd, gluon
 import numpy as np
-import boto3
 import os
 
 ctx = mx.cpu()
 
-
-
-s3_client = boto3.client('s3')    
-bucket_name = os.environ['bucket_name']
 model_name = os.environ['model_name']
-model_path = f'mxnet/base/{model_name}'
+efs_path = '/mnt/efs'
+model_path = efs_path + f'mxnet/base/{model_name}'
 
 image_size = 224
 if model_name == "inception_v3":
@@ -26,13 +22,9 @@ image_classification_shape_type = {
     "mxnet" : (channel, image_size, image_size),
     "tf" : (image_size, image_size, channel)
 }
-def get_model(bucket_name, model_path, model_name):
-    s3_client.download_file(bucket_name, model_path + '/model.json', '/tmp/'+ 'model.json')
-    s3_client.download_file(bucket_name, model_path + '/model.params', '/tmp/'+ 'model.params')
-    return '/tmp/'+ 'model.json', '/tmp/'+ 'model.params'
 
 load_start = time.time()
-model_json, model_params = get_model(bucket_name, model_path, model_name)
+model_json, model_params = model_path + '/model.json', model_path + '/model.params'
 model = gluon.nn.SymbolBlock.imports(model_json, ['data'], model_params, ctx=ctx)
 load_time = time.time() - load_start
 
@@ -63,12 +55,7 @@ def make_dataset(batch_size, workload, framework):
 
 def lambda_handler(event, context):
     event = event['body-json']
-    bucket_name = event['bucket_name']
     batch_size = event['batch_size']
-    arch_type = event['arch_type']
-    framework = event['framework']
-    model_name = event['model_name']
-    compiler = 'base'
     workload = event['workload']
     
     data_start = time.time()
