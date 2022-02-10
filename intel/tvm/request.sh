193575a2aeed9fa45b1722fc0052bf1be44d6645
최재강 <chl8273@naver.com>
2022-02-10 17:02:05 +0900

    check shell time

--- intel/tvm/request.sh
+++ intel/tvm/request.sh
@@ -1,10 +1,17 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/tvm"
+function_name='jg-tvm'
 
+aws lambda update-function-configuration \
+    --function-name $function_name \
+    --environment Variables="{model_name=$1}"
 SET=$(seq 0 4)
 for i in $SET
 do
+start=`date +%s`
 curl -X POST -H 'Content-Type: application/json' \
-    -d '{ "bucket_name" : "dl-converted-models", "batch_size": 1, "arch_type": "intel", "framework": "mxnet", "model_name": "mobilenet_v2_1.tar", "workload": "image_classification" }' \
+    -d '{"batch_size": 1, "workload": "image_classification" }' \
     $API_URL
     echo ""
+end=`date +%s`
+runtime=$((end-start))
 done
