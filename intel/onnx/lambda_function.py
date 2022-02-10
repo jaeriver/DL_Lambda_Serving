9a638d1d890c6d0d2acdc7d187e9dacfea619698
최재강 <chl8273@naver.com>
2022-02-10 16:38:55 +0900

    Update lambda_function.py

--- intel/onnx/lambda_function.py
+++ intel/onnx/lambda_function.py
@@ -1,9 +1,7 @@
 import time
-total_start = time.time()
 import numpy as np
 import onnxruntime as ort
 import os
-
 
 model_name = os.environ['model_name']
 efs_path = '/mnt/efs/'
@@ -56,14 +54,12 @@ def lambda_handler(event, context):
     
     inname = [input.name for input in session.get_inputs()]
     outname = [output.name for output in session.get_outputs()]
-    data_start = time.time()
     if workload == "image_classification":
         data, image_shape = make_dataset(batch_size, workload, framework)
         input_name = "data"
     #case bert
     else:
         data, token_types, valid_length = make_dataset(batch_size, workload, framework)
-    data_time = time.time() - data_start
     start_time = time.time()
     if workload == "image_classification":
         session.run(outname, {inname[0]: data})
@@ -72,6 +68,6 @@ def lambda_handler(event, context):
         session.run(outname, {inname[0]: data,inname[1]:token_types,inname[2]:valid_length})
     running_time = time.time() - start_time
     print(f"ONNX {model_name}-{batch_size} inference latency : ",(running_time)*1000,"ms")
-    total_time = time.time() - total_start
+    
     handler_time = time.time() - handler_start
-    return {'running_time': running_time, 'total_time': total_time, 'data_time':data_time, 'load_time':load_time, 'handler_time': handler_time}
+    return {'handler_time': handler_time, 'load_time': load_time}
