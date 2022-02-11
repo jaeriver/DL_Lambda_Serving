265a152a5afd4e6dacb168ee1afaaea64dde247c
최재강 <chl8273@naver.com>
2022-02-11 14:05:30 +0900

    Update request.sh

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -1,22 +1,32 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/mxnet"
 function_name='jg-mxnet-serving'
 
-aws lambda update-function-configuration \
-    --function-name $function_name \
-    --environment Variables="{model_name=$1}"
-    --memory-size $2
-    
-sleep 30      
-    
-SET=$(seq 0 4)
-for i in $SET
+models=("mobilenet" "mobilenet_v2" "inception_v3" "resnet50" "alexnet" "vgg16" "vgg19")
+memorys=("512" "1024" "2048" "4096" "8192")
+
+for mem in $memorys
 do
-start=`date +%s`
-curl -X POST -H 'Content-Type: application/json' \
-    -d '{"batch_size": 1, "workload": "image_classification" }' \
-    $API_URL
-    echo ""
-end=`date +%s`
-runtime=$((end-start))
-echo "API runtime" $runtime
+    for m in $models
+    do
+        aws lambda update-function-configuration \
+            --function-name $function_name \
+            --environment Variables="{model_name=$m}"
+            --memory-size $mem
+        sleep 60
+
+        SET=$(seq 0 4)
+        for i in $SET
+        do
+        echo $m "performance"
+        echo "----------------"
+        start=`date +%s.%N`
+        curl -X POST -H 'Content-Type: application/json' \
+            -d '{"batch_size": 1, "workload": "image_classification" }' \
+            $API_URL
+            echo ""
+        end=`date +%s.%N`
+        runtime=$((end-start))
+        echo "API runtime" $runtime
+        done
+    done
 done
