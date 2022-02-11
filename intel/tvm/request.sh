016ea8aa4360f778351685227f65be1daca1c77d
최재강 <chl8273@naver.com>
2022-02-11 14:02:58 +0900

    실험 자동화

--- intel/tvm/request.sh
+++ intel/tvm/request.sh
@@ -1,22 +1,32 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/tvm"
 function_name='jg-tvm'
 
-aws lambda update-function-configuration \
-    --function-name $function_name \
-    --environment Variables="{model_name=$1}"
-    --memory-size $2
-    
-sleep 30    
-    
-SET=$(seq 0 4)
-for i in $SET
+models=("mobilenet_1.tar" "mobilenet_v2_1.tar" "inception_v3_1.tar" "resnet50_1.tar" "alexnet_1.tar" "vgg16_1.tar" "vgg19_1.tar")
+memorys=("512" "1024" "2048" "4096" "8192")
+
+for mem in $memorys
 do
-start=`date +%s`
-curl -X POST -H 'Content-Type: application/json' \
-    -d '{"batch_size": 1, "workload": "image_classification" }' \
-    $API_URL
-    echo ""
-end=`date +%s`
-runtime=$((end-start))
-echo "API runtime" $runtime
+    for m in $models
+    do
+        aws lambda update-function-configuration \
+            --function-name $function_name \
+            --environment Variables="{model_name=$m}"
+            --memory-size $mem
+        sleep 60
+
+        SET=$(seq 0 4)
+        for i in $SET
+        do
+        echo $m "performance"
+        echo "----------------"
+        start=`date +%s.%N`
+        curl -X POST -H 'Content-Type: application/json' \
+            -d '{"batch_size": 1, "workload": "image_classification" }' \
+            $API_URL
+            echo ""
+        end=`date +%s.%N`
+        runtime=$((end-start))
+        echo "API runtime" $runtime
+        done
+    done
 done
