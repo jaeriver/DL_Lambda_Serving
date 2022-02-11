333e5631d0c9aa456c27aaae960b267a6df01fc7
최재강 <chl8273@naver.com>
2022-02-11 14:23:12 +0900

    Update request.sh

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -17,8 +17,8 @@ do
         SET=$(seq 0 4)
         for i in $SET
         do
-        echo $m "performance"
-        echo "----------------"
+        echo $m "performance" >> mxnet.txt
+        echo "----------------" >> mxnet.txt
         start=`date +%s.%N`
         response=$(curl -X POST -H 'Content-Type: application/json' \
             -d '{"batch_size": 1, "workload": "image_classification" }' \
