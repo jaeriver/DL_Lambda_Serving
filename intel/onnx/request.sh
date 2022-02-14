5982f48b3af9bd76227a77c44a84f469a139e675
최재강 <chl8273@naver.com>
2022-02-14 15:17:50 +0900

    txt to csv

--- intel/onnx/request.sh
+++ intel/onnx/request.sh
@@ -1,13 +1,15 @@
-API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/onnx"
-function_name='jg-onnx_serving'
+framwork="onnx"
+
+API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/$framework"
+function_name='jg-mxnet-serving'
 
 models="mobilenet.onnx mobilenet_v2.onnx inception_v3.onnx resnet50.onnx alexnet.onnx vgg16.onnx vgg19.onnx"
+models=$(rev<<<$models)
 memorys="512 1024 2048 4096 8192"
 
+echo "lambda_memory, model_name, hardware, framework, total_time, lambda_time, load_time" >> $framework.csv
 for mem in $memorys
 do
-    echo "Memory:"$mem >> onnx.txt
-    echo "---------------------" >> onnx.txt
     for m in $models
     do
         aws lambda update-function-configuration \
@@ -16,20 +18,19 @@ do
             --memory-size $mem
         sleep 60
         
-        echo $m "performance" >> onnx.txt
-
         SET=$(seq 0 4)
         for i in $SET
         do
+
         start=$(($(date +%s%N)/1000000))
         response=$(curl -X POST -H 'Content-Type: application/json' \
             -d '{"batch_size": 1, "workload": "image_classification" }' \
             $API_URL)
-        echo $response >> onnx.txt
         end=$(($(date +%s%N)/1000000))
         runtime=$((end - start))
-        echo "API runtime" $((runtime / 1000)).$((runtime % 1000)) >> onnx.txt
+        echo "API runtime" $((runtime / 1000)).$((runtime % 1000)) >> mxnet.txt
+        
+        echo $mem, $m, "intel", $framework, $((runtime / 1000)).$((runtime % 1000)), $response >> $framework.csv
         done
-    echo "--------------------------------" >> onnx.txt
     done
 done
