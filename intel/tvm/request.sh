71652281f4bb66915c8a306ea89096ac715c8629
최재강 <chl8273@naver.com>
2022-02-14 15:19:10 +0900

    실험값 csv 기록 포맷

--- intel/tvm/request.sh
+++ intel/tvm/request.sh
@@ -1,13 +1,15 @@
-API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/tvm"
-function_name='jg-tvm'
+framwork="tvm"
+
+API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/$framework"
+function_name='jg-$framework-serving'
 
 models="mobilenet_1.tar mobilenet_v2_1.tar inception_v3_1.tar resnet50_1.tar alexnet_1.tar vgg16_1.tar vgg19_1.tar"
+models=$(rev<<<$models)
 memorys="512 1024 2048 4096 8192"
 
+echo "lambda_memory, model_name, hardware, framework, total_time, lambda_time, load_time" >> $framework.csv
 for mem in $memorys
 do
-    echo "Memory:"$mem >> tvm.txt
-    echo "---------------------" >> tvm.txt
     for m in $models
     do
         aws lambda update-function-configuration \
@@ -15,8 +17,6 @@ do
             --environment Variables="{model_name=$m}" \
             --memory-size $mem
         sleep 60
-
-        echo $m "performance" >> tvm.txt
         
         SET=$(seq 0 4)
         for i in $SET
@@ -24,13 +24,13 @@ do
 
         start=$(($(date +%s%N)/1000000))
         response=$(curl -X POST -H 'Content-Type: application/json' \
-            -d '{"batch_size": 1, "workload": "image_classification", "arch_type": "llvm -mcpu=core-avx2" }' \
+            -d '{"batch_size": 1, "workload": "image_classification" }' \
             $API_URL)
-        echo $response >> tvm.txt
         end=$(($(date +%s%N)/1000000))
         runtime=$((end - start))
-        echo "API runtime" $((runtime / 1000)).$((runtime % 1000)) >> tvm.txt
+        echo "API runtime" $((runtime / 1000)).$((runtime % 1000)) >> mxnet.txt
+        
+        echo $mem, $m, "intel", $framework, $((runtime / 1000)).$((runtime % 1000)), $response >> $framework.csv
         done
-    echo "--------------------------------" >> tvm.txt
     done
 done
