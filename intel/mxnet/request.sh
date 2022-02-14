ab628e7cd6eb611a5c00d2c6bc1fa9bf5d1008cf
최재강 <chl8273@naver.com>
2022-02-14 15:14:45 +0900

    실험값 기록 csv 형식 수정

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -1,13 +1,13 @@
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/mxnet"
 function_name='jg-mxnet-serving'
 
+framwork="mxnet"
 models="mobilenet mobilenet_v2 inception_v3 resnet50 alexnet vgg16 vgg19"
 memorys="512 1024 2048 4096 8192"
 
+echo "lambda_memory, model_name, hardware, framework, total_time, lambda_time, load_time" >> $framework.csv
 for mem in $memorys
 do
-    echo "Memory:"$mem >> mxnet.txt
-    echo "---------------------" >> mxnet.txt
     for m in $models
     do
         aws lambda update-function-configuration \
@@ -15,8 +15,6 @@ do
             --environment Variables="{model_name=$m}" \
             --memory-size $mem
         sleep 60
-        
-        echo $m "performance" >> mxnet.txt
         
         SET=$(seq 0 4)
         for i in $SET
@@ -26,11 +24,11 @@ do
         response=$(curl -X POST -H 'Content-Type: application/json' \
             -d '{"batch_size": 1, "workload": "image_classification" }' \
             $API_URL)
-        echo $response >> mxnet.txt
         end=$(($(date +%s%N)/1000000))
         runtime=$((end - start))
         echo "API runtime" $((runtime / 1000)).$((runtime % 1000)) >> mxnet.txt
+        
+        echo $mem, $m, "intel", $framework, $((runtime / 1000)).$((runtime % 1000)), $response >> $framework.csv
         done
-    echo "--------------------------------" >> mxnet.txt
     done
 done
