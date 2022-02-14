51795e4792d42445239a6741b6ae33ef2997fa36
최재강 <chl8273@naver.com>
2022-02-14 15:32:23 +0900

    Update request.sh

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -1,13 +1,13 @@
 framework="mxnet"
 
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/$framework"
-function_name='jg-$framework-serving'
+function_name='jg-'$framework'-serving'
 
 models="mobilenet mobilenet_v2 inception_v3 resnet50 alexnet vgg16 vgg19"
 models=$(rev<<<$models)
 memorys="512 1024 2048 4096 8192"
 
-echo "lambda_memory, model_name, hardware, framework, total_time, lambda_time, load_time" >> $framework.csv
+echo "lambda_memory, model_name, hardware, framework, total_time, lambda_time, load_time" >> $framework'.csv'
 for mem in $memorys
 do
     for m in $models
@@ -28,9 +28,8 @@ do
             $API_URL)
         end=$(($(date +%s%N)/1000000))
         runtime=$((end - start))
-        echo "API runtime" $((runtime / 1000)).$((runtime % 1000)) >> mxnet.txt
         
-        echo $mem, $m, "intel", $framework, $((runtime / 1000)).$((runtime % 1000)), $response >> $framework.csv
+        echo $mem, $m, "intel", $framework, $((runtime / 1000)).$((runtime % 1000)), $response >> $framework'.csv'
         done
     done
 done
