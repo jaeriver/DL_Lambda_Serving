2e1cc38211f83c083a99b76d1e5cd612ae8c43e4
최재강 <chl8273@naver.com>
2022-02-16 16:24:13 +0900

    Update request.sh

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -24,7 +24,7 @@ do
 
         start=$(($(date +%s%N)/1000000))
         response=$(curl -X POST -H 'Content-Type: multipart/form-data' \
-            -F "@test.jpeg" \
+            -F "data=@test.jpeg" \
             $API_URL)
         end=$(($(date +%s%N)/1000000))
         runtime=$((end - start))
