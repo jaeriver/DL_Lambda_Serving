a591608f133200152c9e0242433c88fab4494dac
최재강 <chl8273@naver.com>
2022-02-16 19:11:56 +0900

    Update lambda_function.py

--- intel/onnx/lambda_function.py
+++ intel/onnx/lambda_function.py
@@ -37,7 +37,10 @@ def make_dataset(multipart_data, workload, framework):
         img = BytesIO(binary_content[0])
         print(img)
         img = Image.open(img)
-        img = img.resize((224,224), Image.ANTIALIAS)
+        if model_name == "inception_v3":
+            img = img.resize((299,299), Image.ANTIALIAS)
+        else:
+            img = img.resize((224,224), Image.ANTIALIAS)
         img = np.array(img).astype("float32")
         data = img.reshape(batch_size, channel, image_size, image_size)
 
