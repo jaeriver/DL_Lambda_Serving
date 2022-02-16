ab9b1fd810b1bed3f933a0d1d514fe1a30114a88
최재강 <chl8273@naver.com>
2022-02-16 13:52:13 +0900

    Update request.sh

--- intel/test/request.sh
+++ intel/test/request.sh
@@ -3,4 +3,5 @@ framework="test"
 API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/"$framework
 
 response=$(curl -X POST -H 'Content-Type: multipart/form-data' \
+    -F "data=@test.jpeg;type=application/json" \
     $API_URL)
