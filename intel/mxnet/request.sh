4082e88545bb0c06c8d6567dcf95dfa54f06e23f
최재강 <chl8273@naver.com>
2022-02-16 14:56:41 +0900

    Update request.sh

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -4,7 +4,7 @@ API_URL="https://jbu3pcymu6.execute-api.us-west-2.amazonaws.com/stage1/"$framewo
 function_name='jg-'$framework'-serving'
 
 models="mobilenet mobilenet_v2 inception_v3 resnet50 alexnet vgg16 vgg19"
-models="vgg19 vgg16 alexnet resnet50 inception_v3 mobilenet_v2 mobilenet"
+models="mobilenet vgg19 vgg16 alexnet resnet50 inception_v3 mobilenet_v2 mobilenet"
 memorys="512 1024 2048 4096 8192"
 
 echo "lambda_memory, model_name, hardware, framework, total_time, lambda_time, load_time" >> $framework'.csv'
