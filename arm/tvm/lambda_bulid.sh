62c689f21216aa8d3f5d3368eb423757689ec09c
최재강 <chl8273@naver.com>
2022-02-19 23:01:52 +0900

    change name arm

--- arm/tvm/lambda_bulid.sh
+++ arm/tvm/lambda_bulid.sh
@@ -1,4 +1,4 @@
-docker build -t tvm_lambda_container . --no-cache
+docker build -t tvm_lambda_container_arm . --no-cache
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
