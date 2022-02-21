3c98728fff612fe2101b3bf12407173a23cfe46c
최재강 <chl8273@naver.com>
2022-02-21 11:06:17 +0900

    test arm model

--- arm/mxnet/request.sh
+++ arm/mxnet/request.sh
@@ -5,6 +5,7 @@ function_name='jg-'$framework'-serving-arm'
 
 models="mobilenet mobilenet_v2 inception_v3 resnet50 alexnet vgg16 vgg19"
 models="vgg19 vgg16 alexnet resnet50 inception_v3 mobilenet_v2 mobilenet"
+models="mobilenet_arm"
 memorys="512 1024 2048 4096 8192"
 
 echo "lambda_memory,model_name,hardware,framework,total_time,load_time,lambda_time" >> $framework'.csv'
