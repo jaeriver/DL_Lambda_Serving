6d156eac5155d9605057700638dadefa5362317f
최재강 <chl8273@naver.com>
2022-02-22 20:17:12 +0900

    add nlp workload

--- intel/mxnet/request.sh
+++ intel/mxnet/request.sh
@@ -5,6 +5,7 @@ function_name='jg-'$framework'-serving'
 
 models="mobilenet mobilenet_v2 inception_v3 resnet50 alexnet vgg16 vgg19"
 models="vgg19 vgg16 alexnet resnet50 inception_v3 mobilenet_v2 mobilenet"
+nlp="bert_base distilbert"
 memorys="512 1024 2048 4096 8192"
 
 echo "lambda_memory,model_name,hardware,framework,total_time,load_time,lambda_time" >> $framework'.csv'
@@ -25,6 +26,29 @@ do
         start=$(($(date +%s%N)/1000000))
         response=$(curl -X POST -H 'Content-Type: multipart/form-data' \
             -F "data=@png/test$i.jpeg" \
+            $API_URL)
+        end=$(($(date +%s%N)/1000000))
+        runtime=$((end - start))
+        
+        echo $mem, $m, "intel", $framework, $((runtime / 1000)).$((runtime % 1000)), $response >> $framework'.csv'
+        done
+    done
+    
+    for m in $nlp
+    do
+        aws lambda update-function-configuration \
+            --function-name $function_name \
+            --environment Variables="{model_name=$m, workload=bert}" \
+            --memory-size $mem
+        sleep 900
+        
+        SET=$(seq 1 100)
+        for i in $SET
+        do
+
+        start=$(($(date +%s%N)/1000000))
+        response=$(curl -X POST -H 'Content-Type: multipart/form-data' \
+            -F "data=@png/test$i.txt" \
             $API_URL)
         end=$(($(date +%s%N)/1000000))
         runtime=$((end - start))
