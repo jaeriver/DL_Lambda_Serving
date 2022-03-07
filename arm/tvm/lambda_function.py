4ac8082d4a2ad77d4b116e19a9bf693f467294e8
최재강 <chl8273@naver.com>
2022-03-07 16:43:18 +0900

    Update lambda_function.py

--- arm/tvm/lambda_function.py
+++ arm/tvm/lambda_function.py
@@ -55,7 +55,7 @@ def make_dataset(multipart_data, workload, framework):
             image_shape = (3, 299, 299)
         data_shape = (batch_size,) + image_shape
         img = np.random.uniform(-1, 1, size=data_shape).astype("float32")
-        data = tvm.nd.array(img, ctx=ctx)
+        data = tvm.nd.array(img, ctx)
 
         print(time.time() - mx_start)
         return data
@@ -78,9 +78,9 @@ def make_dataset(multipart_data, workload, framework):
         dtype = 'float32'
         valid_length = np.asarray([seq_length] * batch_size).astype(dtype)
   
-        inputs_nd = tvm.nd.array(inputs, ctx=ctx)
-        token_types_nd = tvm.nd.array(token_types, ctx=ctx)
-        valid_length_nd = tvm.nd.array(valid_length, ctx=ctx)
+        inputs_nd = tvm.nd.array(inputs, ctx)
+        token_types_nd = tvm.nd.array(token_types, ctx)
+        valid_length_nd = tvm.nd.array(valid_length, ctx)
         print(time.time() - mx_start)
         return inputs_nd, token_types_nd, valid_length_nd
 
