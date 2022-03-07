339b7a2c6cebbcec747b74c36c6b512fecdb4087
최재강 <chl8273@naver.com>
2022-03-07 15:23:48 +0900

    add batch size

--- intel/tvm/lambda_function.py
+++ intel/tvm/lambda_function.py
@@ -12,7 +12,7 @@ from PIL import Image
 from requests_toolbelt.multipart import decoder
 
 model_name = os.environ['model_name']
-batch_size = 1
+batch_size = os.environ['batch_size']
 workload = os.environ['workload']
 
 efs_path = '/mnt/efs/'
@@ -39,53 +39,64 @@ load_start = time.time()
 loaded_lib = tvm.runtime.load_module(model_path)
 module = runtime.GraphModule(loaded_lib["default"](ctx))
 load_time = time.time() - load_start
-
 def make_dataset(multipart_data, workload, framework):
     if workload == "image_classification":
-        binary_content = []
-        for part in multipart_data.parts:
-            binary_content.append(part.content)
-        print(binary_content)
-        img = BytesIO(binary_content[0])
-        print(img)
-        img = Image.open(img)
+        mx_start = time.time()
+#         binary_content = []
+#         for part in multipart_data.parts:
+#             binary_content.append(part.content)
+#         img = BytesIO(binary_content[0])
+#         img = Image.open(img)
+#         if model_name == "inception_v3":
+#             img = img.resize((299,299), Image.ANTIALIAS)
+#         else:
+#             img = img.resize((224,224), Image.ANTIALIAS)
+        image_shape = (3, 224, 224)
         if "inception_v3" in model_name:
-            img = img.resize((299,299), Image.ANTIALIAS)
-        else:
-            img = img.resize((224,224), Image.ANTIALIAS)
-        img = np.array(img).astype('float32')
-        data = img.reshape(batch_size, channel, image_size, image_size)
-        data = tvm.nd.array(data, ctx)
-        
+            image_shape = (3, 299, 299)
+        data_shape = (batch_size,) + image_shape
+        img = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+        data = tvm.nd.array(img, ctx=ctx)
+
+        print(time.time() - mx_start)
         return data
     # case bert
     else:
-        binary_content = []
-        for part in multipart_data.parts:
-            binary_content.append(part.content)
-        d = binary_content[0].split(b'\n\r')[0].decode('utf-8')
-        inputs = np.array([d.split(" ")]).astype('float32')
+        mx_start = time.time()
+#         binary_content = []
+#         for part in multipart_data.parts:
+#             binary_content.append(part.content)
+#         d = binary_content[0].split(b'\n\r')[0].decode('utf-8')
+#         inputs = np.array([d.split(" ")]).astype('float32')
+        dtype = "float32"
+        inputs = np.random.randint(0, 2000, size=(batch_size, seq_length)).astype(dtype)
+        token_types = np.random.uniform(size=(batch_size, seq_length)).astype(dtype)
+        valid_length = np.asarray([seq_length] * batch_size).astype(dtype)
         if "lstm" in model_name:
             inputs = np.transpose(inputs)
+            token_types = np.transpose(token_types)
         seq_length = 128
         dtype = 'float32'
         valid_length = np.asarray([seq_length] * batch_size).astype(dtype)
-        inputs = tvm.nd.array(inputs, ctx)
-        valid_length = tvm.nd.array(valid_length, ctx)
-        return inputs, inputs, valid_length
+  
+        inputs_nd = tvm.nd.array(inputs, ctx=ctx)
+        token_types_nd = tvm.nd.array(token_types, ctx=ctx)
+        valid_length_nd = tvm.nd.array(valid_length, ctx=ctx)
+        print(time.time() - mx_start)
+        return inputs_nd, token_types_nd, valid_length_nd
 
 
 def lambda_handler(event, context):
     handler_start = time.time()
-    body = event['body-json']
-    body = base64.b64decode(body)
-    boundary = body.split(b'\r\n')[0]
-    boundary = boundary.decode('utf-8')
-    content_type = f"multipart/form-data; boundary={boundary}"
-    multipart_data = decoder.MultipartDecoder(body, content_type)
+#     body = event['body-json']
+#     body = base64.b64decode(body)
+#     boundary = body.split(b'\r\n')[0]
+#     boundary = boundary.decode('utf-8')
+#     content_type = f"multipart/form-data; boundary={boundary}"
+#     multipart_data = decoder.MultipartDecoder(body, content_type)
     compiler = 'tvm'
     framework = 'mxnet'   
-
+    multipart_data = ""
     if workload == "image_classification":
         data_start = time.time()
         data = make_dataset(multipart_data, workload, framework)
