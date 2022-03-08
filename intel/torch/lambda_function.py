390125b930bc14122d442bd3e1863474c09bff92
최재강 <chl8273@naver.com>
2022-03-08 13:41:35 +0900

    change model load method

--- intel/torch/lambda_function.py
+++ intel/torch/lambda_function.py
@@ -27,7 +27,8 @@ image_classification_shape_type = {
 }
 
 load_start = time.time()
-model = torch.load(model_path)
+model = model.load_state_dict(torch.load(model_path + '/model_state_dict.pt'))
+model.eval()
 load_time = time.time() - load_start
 
 def make_dataset(multipart_data, workload, framework):
