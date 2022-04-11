3dd1f43c6e91862501300a3e3888b3fb24a051ff
최재강 <chl8273@naver.com>
2022-04-11 10:28:52 +0900

    test arm onnx

--- arm/onnx/lambda_function.py
+++ arm/onnx/lambda_function.py
@@ -8,11 +8,12 @@ from PIL import Image
 from requests_toolbelt.multipart import decoder
 
 model_name = os.environ['model_name']
-batch_size = 1
+batch_size = int(os.environ['batch_size'])
 workload = os.environ['workload']
+framework = os.environ['framework']
 
 efs_path = '/mnt/efs/'
-model_path = efs_path + f'mxnet/onnx/{model_name}'
+model_path = efs_path + f'{framework}/onnx/{model_name}'
 
 image_size = 224
 if "inception_v3" in model_name:
@@ -24,69 +25,82 @@ image_classification_shape_type = {
 }
 
 load_start = time.time()
-session = ort.InferenceSession(model_path)
+sess_options = ort.SessionOptions()
+sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+session = ort.InferenceSession(model_path, sess_options)
 session.get_modelmeta()
 load_time = time.time() - load_start
 
 def make_dataset(multipart_data, workload, framework):
     if workload == "image_classification":
-        binary_content = []
-        for part in multipart_data.parts:
-            binary_content.append(part.content)
-        img = BytesIO(binary_content[0])
-        print(img)
-        img = Image.open(img)
+        mx_start = time.time()
+#         binary_content = []
+#         for part in multipart_data.parts:
+#             binary_content.append(part.content)
+#         img = BytesIO(binary_content[0])
+#         img = Image.open(img)
+#         if model_name == "inception_v3":
+#             img = img.resize((299,299), Image.ANTIALIAS)
+#         else:
+#             img = img.resize((224,224), Image.ANTIALIAS)
+        image_shape = (3, 224, 224)
         if "inception_v3" in model_name:
-            img = img.resize((299,299), Image.ANTIALIAS)
-        else:
-            img = img.resize((224,224), Image.ANTIALIAS)
-        img = np.array(img).astype("float32")
-        data = img.reshape(batch_size, channel, image_size, image_size)
-
+            image_shape = (3, 299, 299)
+        data_shape = (batch_size,) + image_shape
+        data = np.random.uniform(-1, 1, size=data_shape).astype("float32")
+        print(time.time() - mx_start)
         return data
     # case bert
     else:
-        seq_length = 128
-        shape_dict = {
-            "data0": (batch_size, seq_length),
-            "data1": (batch_size, seq_length),
-            "data2": (batch_size,),
-        }
+        mx_start = time.time()
+#         binary_content = []
+#         for part in multipart_data.parts:
+#             binary_content.append(part.content)
+#         d = binary_content[0].split(b'\n\r')[0].decode('utf-8')
+#         inputs = np.array([d.split(" ")]).astype('float32')
         dtype = "float32"
         inputs = np.random.randint(0, 2000, size=(batch_size, seq_length)).astype(dtype)
         token_types = np.random.uniform(size=(batch_size, seq_length)).astype(dtype)
         valid_length = np.asarray([seq_length] * batch_size).astype(dtype)
-        
+        if "lstm" in model_name:
+            inputs = np.transpose(inputs)
+            token_types = np.transpose(token_types)
+        seq_length = 128
+        dtype = 'float32'
+        valid_length = np.asarray([seq_length] * batch_size).astype(dtype)
+  
+        print(time.time() - mx_start)
         return inputs, token_types, valid_length
-
+    
+if workload == "image_classification":
+    data = make_dataset(multipart_data, workload, framework)
+#case bert
+else:
+    data, token_types, valid_length = make_dataset(multipart_data, workload, framework)
 
 def lambda_handler(event, context):
     handler_start = time.time()
     
-    body = event['body-json']
-    body = base64.b64decode(body)
-    boundary = body.split(b'\r\n')[0]
-    boundary = boundary.decode('utf-8')
-    content_type = f"multipart/form-data; boundary={boundary}"
-    multipart_data = decoder.MultipartDecoder(body, content_type)
+#     body = event['body-json']
+#     body = base64.b64decode(body)
+#     boundary = body.split(b'\r\n')[0]
+#     boundary = boundary.decode('utf-8')
+#     content_type = f"multipart/form-data; boundary={boundary}"
+#     multipart_data = decoder.MultipartDecoder(body, content_type)
     
-    framework = 'mxnet'
-    
+    multipart_data = ""
     inname = [input.name for input in session.get_inputs()]
     outname = [output.name for output in session.get_outputs()]
-    if workload == "image_classification":
-        data = make_dataset(multipart_data, workload, framework)
-    #case bert
-    else:
-        data, token_types, valid_length = make_dataset(batch_size, workload, framework)
+
     start_time = time.time()
     if workload == "image_classification":
         session.run(outname, {inname[0]: data})
     # case : bert
-    else:
+    elif "bert_base" in model_name:
         session.run(outname, {inname[0]: data,inname[1]:token_types,inname[2]:valid_length})
+    else:
+        session.run(outname, {inname[0]: data,inname[1]:valid_length})
+        
     running_time = time.time() - start_time
-    print(f"ONNX {model_name}-{batch_size} inference latency : ",(running_time)*1000,"ms")
-    
     handler_time = time.time() - handler_start
     return load_time, handler_time
