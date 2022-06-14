1e72956bd2a8aabfa384e701462f362e6d2b6aae
최재강 <chl8273@naver.com>
2022-06-14 15:46:51 +0900

    model name changed

--- arm/mxnet/lambda_function.py
+++ arm/mxnet/lambda_function.py
@@ -30,7 +30,7 @@ image_classification_shape_type = {
 }
 
 load_start = time.time()
-model_json, model_params = model_path + '/model-symbol.json', model_path + '/model-0000.params'
+model_json, model_params = model_path + '/model.json', model_path + '/model.params'
 if "bert_base" in model_name:
      model = gluon.nn.SymbolBlock.imports(model_json, ['data0','data1','data2'] , model_params, ctx=ctx)
 elif "distilbert" in model_name:
