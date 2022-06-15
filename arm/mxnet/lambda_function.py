041d5c9a6df1aec8052256387649c17273235be5
최재강 <chl8273@naver.com>
2022-06-15 16:36:19 +0900

    add local bert downlaod

--- arm/mxnet/lambda_function.py
+++ arm/mxnet/lambda_function.py
@@ -29,8 +29,70 @@ image_classification_shape_type = {
     "tf" : (image_size, image_size, channel)
 }
 
+def bert_download(model_name,seq_length, batch_size, dtype="float32"):
+    inputs = np.random.randint(0, 2000, size=(batch_size, seq_length)).astype(dtype)
+    token_types = np.random.uniform(size=(batch_size, seq_length)).astype(dtype)
+    valid_length = np.asarray([seq_length] * batch_size).astype(dtype)
+        
+    inputs_nd = mx.nd.array(inputs, ctx=ctx)
+    token_types_nd = mx.nd.array(token_types, ctx=ctx)
+    valid_length_nd = mx.nd.array(valid_length, ctx=ctx)
+
+    # Instantiate a BERT classifier using GluonNLP
+    if model_name == "bert_base":
+        model_name_ = "bert_12_768_12"
+        dataset = "book_corpus_wiki_en_uncased"
+        model, _ = nlp.model.get_model(
+                name=model_name_,
+                dataset_name=dataset,
+                pretrained=True,
+                use_pooler=True,
+                use_decoder=False,
+                use_classifier=False,
+            )
+        model = nlp.model.BERTClassifier(model, dropout=0.1, num_classes=2)
+        model.initialize(ctx=ctx)
+        model.hybridize(static_alloc=True)
+                
+        mx_out = model(inputs_nd, token_types_nd, valid_length_nd)
+        mx_out.wait_to_read()
+
+        # print model info
+        #print("-"*10,f"{model_name} Parameter Info","-"*10)
+        #print(model.summary(inputs_nd,token_types_nd, valid_length_nd))       
+
+    elif model_name == "distilbert":
+        model_name_="distilbert_6_768_12"
+        dataset = "distilbert_book_corpus_wiki_en_uncased"
+        model, _ = nlp.model.get_model(
+                name=model_name_,
+                dataset_name=dataset,
+                pretrained=True,
+            )
+        model.hybridize(static_alloc=True)
+
+        mx_out = model(inputs_nd, valid_length_nd)
+        mx_out.wait_to_read()
+
+        # print("-"*10,f"{model_name} Parameter Info","-"*10)
+        # print(model.summary(inputs_nd, valid_length_nd))
+  
+
+    target_path = f"/tmp/{model_name}"
+    from pathlib import Path
+    Path(target_path).mkdir(parents=True, exist_ok=True)  
+
+    model.export(f'{model_name}/model')
+    print("-"*10,f"Download {model_name} complete","-"*10)  
+
+
+
 load_start = time.time()
 print('test')
+model_path = f'/tmp/{model_name}'
+
+bert_download(model_name, 128, 1)
+
 model_json, model_params = model_path + '/model.json', model_path + '/model.params'
 if "bert_base" in model_name:
      model = gluon.nn.SymbolBlock.imports(model_json, ['data0','data1','data2'] , model_params, ctx=ctx)
